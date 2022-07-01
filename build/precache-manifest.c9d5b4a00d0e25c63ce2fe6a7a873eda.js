5795de11053cdf237947f664cc8586183f608faf
Emmanuel Paul <emmapaul247@gmail.com>
2022-07-01 12:46:01 +0100

    Create production build

---
+++ build/precache-manifest.c9d5b4a00d0e25c63ce2fe6a7a873eda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "10da5b41860fc82d42a4",
+    "url": "/static/css/main.68c9890e.chunk.css"
+  },
+  {
+    "revision": "10da5b41860fc82d42a4",
+    "url": "/static/js/main.10da5b41.chunk.js"
+  },
+  {
+    "revision": "a7a3c97749e7aa6b1852",
+    "url": "/static/js/1.a7a3c977.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b06b2e50e9cd8d079e6128d91cdf750e",
+    "url": "/index.html"
+  }
+];
